c3e6e29e81dc04343fd93725073df426d4bebf2c
Bruno Coelho <br_coelho98@hotmail.com>
2023-08-17 23:43:11 -0300

    Ver 0.2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use std::io;
 use std::string::String;
+use std::fs;
 
 fn main() {
-    let mut departments = HashMap::new();
+    let mut departments = load_departments();
 
     loop {
         let mut input = String::new();
@@ -17,7 +18,10 @@ fn main() {
                     let name = command[1..index].join(" ");
                     let department = command[index + 1].clone();
 
-                    departments.entry(department).or_insert(Vec::new()).push(name);
+                    departments.entry(department.clone()).or_insert(Vec::new()).push(name.clone());
+                    save_departments(&departments);
+
+                    println!("Added {} to {} department.", name, department);
                 } else {
                     println!("Error: Please specify a department.");
                 }
@@ -46,4 +50,17 @@ fn main() {
             _ => println!("Unknown command: {}", &command[0]),
         }
     }
+}
+
+fn load_departments() -> HashMap<String, Vec<String>> {
+    if let Ok(contents) = fs::read_to_string("departments.txt") {
+        serde_json::from_str(&contents).unwrap_or(HashMap::new())
+    } else {
+        HashMap::new()
+    }
+}
+
+fn save_departments(departments: &HashMap<String, Vec<String>>) {
+    let serialized = serde_json::to_string(departments).unwrap();
+    fs::write("departments.txt", serialized).unwrap();
 }
